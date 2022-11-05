c2ed5f62131a03567d244096537f8b2ca0694502
camisbrussi <camila.sbrussi@universo.univates.br>
2022-11-05 18:13:30 -0300

    first commit

---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,46 @@
+import { TouchableOpacity, View, Text } from "react-native";
+import { GameController } from "phosphor-react-native";
+import { THEME } from "../../theme";
+
+import { DuoInfo } from "../DuoInfo";
+
+import { styles } from "./styles";
+
+export interface DuoCardProps {
+  hourEnd: string;
+  hourStart: string;
+  id: string;
+  name: string;
+  useVoiceChannel: boolean;
+  weekDays: string[];
+  yearsPlaying: number;
+}
+
+interface Props {
+  data: DuoCardProps;
+  onConnect: () => void;
+}
+
+export function DuoCard({ data, onConnect }: Props) {
+  return (
+    <View style={styles.container}>
+      <DuoInfo label="Nome" value={data.name} />
+      <DuoInfo label="Tempo de Jogo" value={`${data.yearsPlaying} ano(s)`} />
+      <DuoInfo
+        label="Disponibilidade"
+        value={`${data.weekDays.length} dia(s) \u2022 ${data.hourStart} - ${data.hourEnd}`}
+      />
+      <DuoInfo
+        label="Chamada de Aúdio"
+        value={data.useVoiceChannel ? "Sim" : "Não"}
+        colorValue={
+          data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT
+        }
+      />
+      <TouchableOpacity style={styles.button} onPress={onConnect}>
+        <GameController color={THEME.COLORS.TEXT} size={20} />
+        <Text style={styles.buttonTitle}>Conectar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Image, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+
+import { Background } from "../../components/Background";
+import { GameCard, GameCardProps } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
+
+import { styles } from "./styles";
+
+export function Home() {
+  const [games, setGame] = useState<GameCardProps[]>([]);
+
+  const navigation = useNavigation();
+  useEffect(() => {
+    fetch("http://192.168.1.133:3333/games")
+      .then((response) => response.json())
+      .then((data) => setGame(data));
+  }, []);
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate("game", { id, title, bannerUrl });
+  }
+
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logoImg} style={styles.logo} />
+        <Heading
+          title="Encontre seu duo!"
+          subtitle="Selecione o game que deseja jogar..."
+        />
+
+        <FlatList
+          data={games}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <GameCard data={item} onPress={() => handleOpenGame(item)} />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.contentList}
+        />
+      </SafeAreaView>
+    </Background>
+  );
+}
